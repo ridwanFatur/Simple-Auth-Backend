ab549b310a064242109807b56bd1caa6eeb41ddc
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2025-04-08 18:44:15 +0700

    Add function on test-task for debug

--- core/management/commands/test_tasks.py
+++ core/management/commands/test_tasks.py
@@ -1,3 +1,6 @@
+import os
+import re
+
 from django.core.management import BaseCommand
 from django.template.loader import render_to_string
 
@@ -8,9 +11,27 @@ from users.models import UserModel
 
 class Command(BaseCommand):
     def execute(self, *args, **options):
-        test_send_email_verification()
+        get_all_requirements()
+        # test_send_email_verification()
         # test_send_email()
         pass
+
+
+def get_all_requirements():
+    imports = set()
+
+    for root, dirs, files in os.walk("."):
+        for file in files:
+            if file.endswith(".py"):
+                file_path = os.path.join(root, file)
+                with open(file_path, "r", encoding="utf-8") as f:
+                    for line in f:
+                        line = line.strip()
+                        if re.match(r"^(from|import) ", line):
+                            imports.add(line)
+
+    for imp in sorted(imports):
+        print(imp)
 
 
 def test_send_email_verification():
