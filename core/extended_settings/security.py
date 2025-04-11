df9c9eadea42e402b6f061c41e80358a25515e30
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2025-04-11 23:47:55 +0700

    Add CORS_ALLOWED_ORIGINS

--- core/extended_settings/security.py
+++ core/extended_settings/security.py
@@ -3,6 +3,8 @@ import os
 DEBUG = os.environ.get("DEBUG").lower() in ("true", "1", "t")
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 CSRF_TRUSTED_ORIGINS = [f"https://{host}" for host in ALLOWED_HOSTS]
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS").split(",")
+
 SECRET_KEY = os.environ.get("SECRET_KEY")
 SITE_URL = os.getenv("SITE_URL")
 
