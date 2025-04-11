a054b0dd86b366180c37e2ce92772b0d15118b33
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2025-04-11 20:50:12 +0700

    Update CSRF_TRUSTED_ORIGINS

--- core/extended_settings/security.py
+++ core/extended_settings/security.py
@@ -2,6 +2,7 @@ import os
 
 DEBUG = os.environ.get("DEBUG").lower() in ("true", "1", "t")
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
+CSRF_TRUSTED_ORIGINS = [f"https://{host}" for host in ALLOWED_HOSTS]
 SECRET_KEY = os.environ.get("SECRET_KEY")
 SITE_URL = os.getenv("SITE_URL")
 
